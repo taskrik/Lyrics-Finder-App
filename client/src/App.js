6d31bbf32fd8ae47c50c9a78d67e4ea46417c0e0
tasos <tasos.krikonis@gmail.com>
2018-09-09 16:22:39 +0200

    final touches

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
@@ -11,9 +10,6 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
           <Navbar />
           <div className="container">
             <Switch>
