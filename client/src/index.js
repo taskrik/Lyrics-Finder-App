fe7efa6c2be053f767691d0631dd43fe9e21dff7
tasos <tasos.krikonis@gmail.com>
2018-09-15 18:10:07 +0200

    serv-worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+//import registerServiceWorker from './registerServiceWorker'
 import store from './store'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
@@ -16,4 +16,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-registerServiceWorker();
+//registerServiceWorker();
