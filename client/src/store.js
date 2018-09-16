da1070650c87243ab6629a6b053c9fc03a500ac4
tasos <tasos.krikonis@gmail.com>
2018-09-16 12:29:30 +0200

    same thing

--- client/src/store.js
+++ client/src/store.js
@@ -2,11 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers/index'
 import ReduxThunk from 'redux-thunk'
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 
 const enhancer = compose(
   applyMiddleware(ReduxThunk),
-  devTools
+  
 )
 
 const store = createStore(reducer, enhancer)
